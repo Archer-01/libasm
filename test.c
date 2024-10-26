6dc70ffb8b0135f73acd94cba7b18b20379e0426
Hamza Haddani <hhamza@student.1337.ma>
2024-10-26 12:52:07 +0100

    feat: Add ft_write

--- test.c
+++ test.c
@@ -1,9 +1,12 @@
 #include "libasm.h"
 #include <assert.h>
+#include <errno.h>
+#include <fcntl.h>
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #define TEST_STRLEN(s) assert(strlen(s) == ft_strlen(s))
 #define TEST_STRCPY(dst, src) assert(strcmp(strcpy(dst, src), ft_strcpy(dst, src)) == 0)
@@ -47,6 +50,30 @@ int main(void)
 		TEST_STRCMP("abcdf", "abcde");
 		TEST_STRCMP("abc", "abcdef");
 		TEST_STRCMP("abcdef", "abc");
+	}
+
+	// ft_write
+	{
+		/* Successful call */ {
+			const char *message = "libasm";
+			const int fd = open("/dev/null", O_WRONLY, 0);
+			const ssize_t ret = ft_write(fd, message, strlen(message));
+			assert(ret == strlen(message));
+			close(fd);
+		}
+
+		/* Bad file descriptor */ {
+			const char *message = "libasm";
+			const ssize_t ret = ft_write(3, message, strlen(message));
+			assert(ret == -1);
+			assert(errno == EBADF);
+		}
+
+		/* Bad buffer */ {
+			const ssize_t ret = ft_write(STDOUT_FILENO, NULL, 42);
+			assert(ret == -1);
+			assert(errno == EFAULT);
+		}
 	}
 
 	puts("All tests passed");
